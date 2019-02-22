23d63e022d67187a311570e69b36eea9b2f8a1a7
rlama7 <royalray7@gmail.com>
2019-02-21 20:12:22 -0800

    Migrated old server to a new express server to resolve reverse proxy issue.

--- Milestones/M0/app.js
+++ Milestones/M0/app.js
@@ -1,18 +1,50 @@
-// jshint esverison: 6
-
+// jshint esversion:6
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
 const app = express();
-const port = 3000;
+const port = 80;
 
+// middleware
+hbs.registerPartials(__dirname + 'views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
 
-// index/homepage routing
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+// log
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = '${now}: ${req.method} ${req.url}';
+
+  console.log();
+  fs.appendFile('server.log', log + '\n');
+  next();
+});
+
+// hbs helper function
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text)=> {
+  return text.toUpperCase();
+});
+
+// homepae (index) routing
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Software Engineering class SFSU',
+    semesterYear: 'Spring 2019',
+    section: '01',
+    team: 'Team007',
+  });
 });
 
 // about: Ratna Lama
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/ratnalama.html');
+app.get('/ratnalama', (req, res) => {
+  res.render('ratnalama.hbs', {
+    pageTitle: 'About Me: ',
+  });
 });
 
 // about: Hadia Andar
@@ -42,5 +74,5 @@ app.get('/', function (req, res) {
 
 // listen from express module
 app.listen(port, function () {
-  console.log('App listening on port ${port}!...');
+  console.log('App listening on port ' + port + ' ...');
 });
