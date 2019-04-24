1a7477b9e7edeb31dab4d6690ac850415ce81890
adedamol <damoladetayo@gmail.com>
2019-04-24 01:19:27 -0700

    UI design implementation

---
+++ Oasis/create_db.js
@@ -0,0 +1,28 @@
+/**
+ * CREATE A MySQL DATABASE AND A TABLE IN DBMS
+ */
+
+var mysql = require('mysql');
+
+
+// Establish Database Connection
+var db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: '',
+});
+
+// Connect to MySQL
+db.connect(function(err) {
+    if(err) throw err;
+    console.log("Connection established to MySQL DBMS successfully...");
+
+    // CREATE DATABASE
+    var sql = "CREATE DATABASE property";
+    db.query(sql, function(err, result, field) {
+        if(err) throw err;
+        console.log("Database Created Successfully...");
+    });
+
+});
