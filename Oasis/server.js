b6162532a5c6f5a96461ef44c5b049948443f16c
rlama7 <royalray7@gmail.com>
2019-05-17 19:40:57 -0700

    Updated acknowledgement to the renter.

--- Oasis/server.js
+++ Oasis/server.js
@@ -64,7 +64,7 @@ app.post("/send", (req, res) => {
     console.log("Message sent: %s", info.messageId);
     console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 
-    res.render("contact", { msg: "Email has been sent!" });
+    res.render("thankyourenter");
   });
 });
 
